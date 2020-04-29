d9e6ba3cca6e83c62a2e0392a12b668b49d54398
mrueda-transom <50331678+mrueda-transom@users.noreply.github.com>
2020-04-29 10:22:26 -0500

    Add files via upload

--- projects/dev-banner/src/app/app.component.ts
+++ projects/dev-banner/src/app/app.component.ts
@@ -10,22 +10,24 @@ import { map, first } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  banner: any;
+  banner: any = [];
+  top: any;
 
   constructor(
     private sis: SharepointIntegrationService
   ) { }
 
   ngOnInit() {
-    this.loadBanner();
+    this.loadBannerMaster();
   }
 
 
-  private loadBanner() {
+  private loadBannerMaster() {
     const data = {
       select: ['Id', 'Title', 'Orden', 'Descripcion', 'Enlace', 'Imagen', 'Subtitulo', 'Activarboton', 'Palabrasclave', 'Dependencia',
-              'Textodelboton', 'Posiciontexto', 'Enlaceboton', 'Activartitulo', 'subSitios'],
-      orderBy: 'Orden',
+              'Textodelboton', 'Posiciontexto', 'Enlaceboton', 'Activartitulo', 'subSitios', 'BannerPrincipal', 'Created'],
+      orderBy: 'Created',
+      reverse: true,
       top: 20
     };
 
@@ -36,7 +38,7 @@ export class AppComponent implements OnInit {
             return {
               created: new Date(r.Created),
               id: r.Id,
-              image: null,
+              image: r.Imagen,
               title: r.Title,
               description: r.Descripcion,
               enlace: r.Enlace,
@@ -47,19 +49,32 @@ export class AppComponent implements OnInit {
               posicionTexto: r.Posiciontexto,
               enlaceBoton: r.Enlaceboton,
               activarTitulo: r.Activartitulo,
-              subSitios: r.subSitios
+              subSitios: r.subSitios,
+              principal: r.BannerPrincipal,
             };
           })
         )
       )
       .subscribe(response => {
-        this.banner = response;
-
-        this.loadImages();
+        if (response) {
+          response.forEach(element => {
+            if (element.principal) {
+              this.banner.push(element);
+            }
+          });
+          this.top = 20 - this.banner.length;
+          console.log('Master');
+          console.log(this.banner.length);
+          console.log(this.top);
+          this.loadBannerLocal();
+        } else {
+            this.top = 20;
+            this.loadBannerLocal();
+        }
       });
   }
 
-  private loadImages() {
+  private loadImagesMaster() {
     const data = {
       select: ['Imagen']
     };
@@ -78,6 +93,47 @@ export class AppComponent implements OnInit {
       .subscribe((response: any) => {
         response.forEach((r, index) => {
           this.banner[index].image = r.Imagen;
+        });
+      });
+  }
+
+  private loadBannerLocal() {
+    const data2 = {
+      select: ['Id', 'Title', 'Orden', 'Descripcion', 'Enlace', 'Imagen', 'Subtitulo', 'Activarboton', 'Palabrasclave', 'Dependencia',
+              'Textodelboton', 'Posiciontexto', 'Enlaceboton', 'Activartitulo', 'subSitios', 'BannerPrincipal', 'Created'],
+      filter: ['BannerPrincipal eq false'],
+      orderBy: 'Orden',
+      top: this.top,
+    };
+
+    this.sis.read('Banner', data2)
+      .pipe(
+        map((response: any) =>
+          response.value.map(r => {
+            return {
+              created: new Date(r.Created),
+              id: r.Id,
+              order: r.Orden,
+              image: r.Imagen,
+              title: r.Title,
+              description: r.Descripcion,
+              enlace: r.Enlace,
+              subtitulo: r.Subtitulo,
+              activarBoton: r.Activarboton,
+              dependencia: r.Dependencia,
+              textoBoton: r.Textodelboton,
+              posicionTexto: r.Posiciontexto,
+              enlaceBoton: r.Enlaceboton,
+              activarTitulo: r.Activartitulo,
+              subSitios: r.subSitios,
+              principal: r.BannerPrincipal,
+            };
+          })
+        )
+      )
+      .subscribe(response => {
+        response.forEach(element => {
+          this.banner.push(element);
         });
       });
   }
